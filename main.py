1277b5ea84b5765310414b6661080134acca8998
Robbie McNeil <36566724+mcneil1@users.noreply.github.com>
2021-10-26 22:29:38 +0000

    Changed to now use commands

--- main.py
+++ main.py
@@ -3,24 +3,22 @@ import discord
 import re
 import random
 from keep_alive import keep_alive
+from discord.ext import commands
 
-client = discord.Client()
+intents = discord.Intents.default()
 
-@client.event
+bot = commands.Bot(command_prefix=os.environ['prefix'], intents=intents)
+
+@bot.event
 async def on_ready():
-  print('{0.user} is online, time to graft.'.format(client))
+  print("Dezzie is online, time to graft.")
+  await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="Grafting"))
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
-  if message.content.startswith('/r '):
-
-    x = re.search("\/r \d+d\d+", message.content)
+@bot.command()
+async def r(ctx, arg):
+    x = re.search("\d+d\d+", arg)
     if x:
-      y = re.split(" ", message.content)
-      roll = y[1]
+      roll = arg
       rollSplit = re.split("d", roll)
       numberOfDie = int(rollSplit[0])
       numberOfSides = int(rollSplit[1])
@@ -28,11 +26,11 @@ async def on_message(message):
       total = 0
 
       if numberOfDie > 10:
-        await message.channel.send("Woah there lad, calm down. I can only roll 10 dice at a time.")
+        await ctx.send("Woah there lad, calm down. I can only roll 10 dice at a time.")
       elif numberOfSides > 100:
-        await message.channel.send("Steady on fella, I can't roll any higher than a d100")
+        await ctx.send("Steady on fella, I can't roll any higher than a d100")
       else:
-        await message.channel.send("rolling "+ str(numberOfDie) + " x " + "d" + str(numberOfSides) + "...")
+        await ctx.send("rolling "+ str(numberOfDie) + " x " + "d" + str(numberOfSides) + "...")
 
         for x in range(numberOfDie):
           dieResult = random.randint(1, numberOfSides)
@@ -42,16 +40,12 @@ async def on_message(message):
           else:
             outcome = outcome + " " + str(dieResult) + " +"
 
-        await message.channel.send(outcome)
+        await ctx.send(outcome)
         
         if numberOfDie > 1 :
-          await message.channel.send("total = " + str(total))
+          await ctx.send("total = " + str(total))
     else:
-      await message.channel.send("Invalid syntax. Use /help for info on how to use DezzieBot")
-
-
-  if message.content.startswith("/help"):
-    await message.channel.send("Commands:\n/r - this will roll a chosen amount of dice with a chosen amount of sides.\nExample:  '/r 2d6'  - this will roll two six-sided die")
+      await ctx.send("Invalid syntax. Use $help for info on how to use DezzieBot")
 
 keep_alive()    
-client.run(os.environ['botToken'])
+bot.run(os.environ['botToken'])
