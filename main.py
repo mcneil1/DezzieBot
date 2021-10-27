3bc8a299a1e1d25e68c6afc6123375fe6bcf1f19
Robbie McNeil <36566724+mcneil1@users.noreply.github.com>
2021-10-27 00:10:52 +0000

    roll command can handle a modifier, help function added

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 
+
+
+
+
+
+
+
 bot = commands.Bot(command_prefix=os.environ['prefix'], intents=intents)
 
 @bot.event
@@ -15,14 +22,26 @@ async def on_ready():
   await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="Grafting"))
 
 @bot.command()
-async def r(ctx, arg):
-    x = re.search("\d+d\d+", arg)
+async def r(ctx, *args):
+  try:
+    x = re.search("\d+d\d+$", args[0])
+    
+    index = 1
+    secondArg = index < len(args)
+
+    add = 0
+    subtract = 0
+
+    if secondArg:
+      subtract = re.search("-\d+", args[1])
+      add = re.search("\+\d+", args[1])
+
     if x:
-      roll = arg
+      roll = x.string
       rollSplit = re.split("d", roll)
       numberOfDie = int(rollSplit[0])
       numberOfSides = int(rollSplit[1])
-      outcome = "="
+      outcome = "= ("
       total = 0
 
       if numberOfDie > 10:
@@ -36,16 +55,43 @@ async def r(ctx, arg):
           dieResult = random.randint(1, numberOfSides)
           total = total + dieResult
           if x == numberOfDie-1:
-            outcome = outcome + " " +str(dieResult)
+            outcome = outcome + " " +str(dieResult)+ " )"
           else:
             outcome = outcome + " " + str(dieResult) + " +"
+
+
+        if subtract:
+          y = args[1]
+          num = int(y[1:])
+
+          outcome = outcome + " - " + str(num)
+          total = total - num
+
+        if add:
+          y = args[1]
+          num = int(y[1:])
+
+          outcome = outcome + " + " + str(num)
+          total = total + num
+
+
 
         await ctx.send(outcome)
         
         if numberOfDie > 1 :
           await ctx.send("total = " + str(total))
+        elif subtract or add:
+          await ctx.send("total = " + str(total))
+
+    elif args[0] == "help":
+      await ctx.send(".r - specify the number of die and the type of die you would like to roll. You can roll up to 10 die and you can roll from a 1 sided die to a 100 sided die. You can also add a modifier to add or subtract from the outcome. Note: only the first modifier added will be taken into account .\n\nExample: '$r 2d6 +4' - this will roll 2 six sided die and add 4 onto the total.")
     else:
-      await ctx.send("Invalid syntax. Use $help for info on how to use DezzieBot")
+      await ctx.send("Invalid syntax. Use '.r help' for info on how to use roll command")
+  except:
+    await ctx.send("An error has occurred. Use '.r help' for info on how to use roll command")
+
+
+
 
 keep_alive()    
 bot.run(os.environ['botToken'])
